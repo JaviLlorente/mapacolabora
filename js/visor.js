93ec0a58c2a17066343ba25dd89c01eeac01edbe
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2021-02-10 09:56:38 +0100

    Update visor.js

--- js/visor.js
+++ js/visor.js
@@ -4,7 +4,10 @@ var mapa = new L.map('mi-mapa', {
 });
 
 var capaOSM = new L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png');
-var capaRelieve = new L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg');
+//var capaRelieve = new L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg');
+var capaHibrido = new L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{	
+			attribution: 'Mapa híbrido de Google',
+			maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3'] });
 
 capaRelieve.addTo(mapa);
 //capaOSM.addTo(mapa);
