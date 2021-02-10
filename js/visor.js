3e2565cd508241f751383e990ec2576b96ff57e4
JaviLlorente <58568865+JaviLlorente@users.noreply.github.com>
2021-02-10 09:18:00 +0100

    Add files via upload

---
+++ js/visor.js
@@ -0,0 +1,19 @@
+var mapa = new L.map('mi-mapa', {
+    center: [20, 50],
+    zoom: 2    
+});
+
+var capaOSM = new L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png');
+var capaRelieve = new L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg');
+
+capaRelieve.addTo(mapa);
+//capaOSM.addTo(mapa);
+
+
+var capasBase = {
+    "Relieve": capaRelieve,
+    "OpenStreetMap": capaOSM
+};
+
+var selectorCapas = new L.control.layers(capasBase);
+selectorCapas.addTo(mapa);
